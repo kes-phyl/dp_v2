cfdaedae424466540e2cbe9745550bc19e4ac8cc
kes-phyl <danphylbert@gmail.com>
2024-02-26 22:21:27 +0000

    add cv, Link button cv

--- components/AnimatedButton.tsx
+++ components/AnimatedButton.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import Link from 'next/link'
 
 const AnimatedButton = () => {
   return (
+  <Link href='/Dan_Phylbert_Cv.pdf'>
     <button className="button-animated mt-10" data-text="Awesome">
     <span className="actual-text">&nbsp;Resume?&nbsp;</span>
     <span aria-hidden="true" className="hover-text">&nbsp;Resume?&nbsp;</span>
-</button>
+    </button>
+</Link>
   )
 }
 
