a08f5a319e069c0733cd77d6a5d864c3a7495a5a
kes-phyl <danphylbert@gmail.com>
2024-02-28 12:22:20 +0000

    update resume button

--- components/AnimatedButton.tsx
+++ components/AnimatedButton.tsx
@@ -2,12 +2,15 @@ import Link from 'next/link'
 
 const AnimatedButton = () => {
   return (
-  <Link className="button-animated mt-10" data-text="Awesome" target='_blank' href='/DanPhylbertCv.pdf'>
+    <button className="button-animated mt-10" data-text="Awesome">
+  <Link target='_blank' href='/DanPhylbertCv.pdf'>
     {/* <button className="button-animated mt-10" data-text="Awesome"> */}
     <span className="actual-text">&nbsp;Resume?&nbsp;</span><span aria-hidden="true" className="hover-text">&nbsp;Resume?&nbsp;</span>
     {/* <span aria-hidden="true" className="hover-text">&nbsp;Resume?&nbsp;</span> */}
     {/* </button> */}
+   
 </Link>
+</button>
   )
 }
 
